fda3d3301a1be14fa889d1deefd974eccabdf065
atul patil <atulpatil539@gmail.com>
2021-12-22 23:13:09 +0530

    refactor: product component added

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
+import Product from './Product';
+
+
+const Products = [
+    { id: 1 , name: "shoes", description: "Running shoes" },
+    { id:2, name: "mackbook", description: "Apple macbook"}
+]
 
 const Products = () =>{
 
       <main>
-          <Grid container justify="center"  spacing={4} ></Grid>
+          <Grid container justify="center"  spacing={4} >
+          {products.map((product) => (
+          <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}>
+            <Product />
+          </Grid>
+        ))}
+          </Grid>
       </main>
 }
 
