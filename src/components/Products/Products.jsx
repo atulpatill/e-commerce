9d0acb63986fb3d49a99e3ec1970a4d6a0637144
atul patil <atulpatil539@gmail.com>
2021-12-29 23:30:41 +0530

    style: product components

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import Grid from '@material-ui/core/Grid';
+import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 
 
-const Products = [
+const products = [
     { id: 1 , name: "shoes", description: "Running shoes", price:"$5" },
     { id:2, name: "mackbook", description: "Apple macbook", price:"$10"}
-]
+];
 
 const Products = () =>{
-
+return (
       <main>
           <Grid container justify="center"  spacing={4} >
           {products.map((product) => (
           <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}>
-            <Product />
+            <Product  product={product} />
           </Grid>
         ))}
           </Grid>
       </main>
+);
 }
 
 export default Products;
