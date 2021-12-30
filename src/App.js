6f50a98ec9aca556dc554835b346e9b31c5d059d
atul patil <atulpatil539@gmail.com>
2021-12-30 23:31:48 +0530

    feat: Navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Navbar from './components/Products/Navbar/Navbar'
+
 import Products from './components/Products/Products'
 
 function App() {
   return (
     <div>
+      <Navbar />
     <Products />
     </div>
   )
